e88cdde1227ba7b1d6f7968974c81d2195bf55f6
Felipe Silva <felipe_zanini@hotmail.com>
2023-10-07 00:45:46 +0000

    initals commits

---
+++ blog/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('tile',)}
+    list_filter = {'status', 'created_on'}
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    summernote_fields = ('content')
+
+
+@admin.register(Comment):
+class CommentAdmin()
+
+
